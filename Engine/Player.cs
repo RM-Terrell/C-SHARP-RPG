041d431a98f076451a6b059a2c6a54dbe7de22de
RM-Terrell <terrellsystems@gmail.com>
2017-10-01 13:38:39 -0600

    removed unneeded using statements

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -1,11 +1,9 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using System.ComponentModel;
+using System.Linq;
+using System.Xml;
 
 namespace Engine
 {
